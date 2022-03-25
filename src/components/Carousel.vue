f782b2696467f6c59f53a8c354f8cda0cf2fb6ef
Mahier Sydow <mahier.sydow8@gmail.com>
2022-03-25 14:54:24 +0900

    fixed the format by moving everything from app.vue to home.vue then added the router to the index.js

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -7,3 +7,89 @@ export default {};
 </script>
 
 <style></style>
+<template>
+  <div class="carousel">
+    <slot :currentSlide="currentSlide" />
+
+    <!-- Navigation -->
+    <div class="navigate">
+      <div class="toggle-page left">
+        <i @click="prevSlide" class="fa-solid fa-chevron-left"></i>
+      </div>
+      <div class="toggle-page right">
+        <i @click="nextSlide" class="fa-solid fa-chevron-right"></i>
+      </div>
+    </div>
+
+    <!-- Pagination -->
+    <div class="pagination">
+      <span
+        @click="goToSlide(index)"
+        v-for="(slide, index) in getSlideCount"
+        :key="index"
+        :class="{ active: index + 1 === currentSlide }"
+      >
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+export default {
+  setup() {
+    const currentSlide = ref(1);
+    const getSlideCount = ref(null);
+    const autoPlayEnabled = ref(true);
+    const timeoutDuration = ref(5000);
+
+    // next slide
+    const nextSlide = () => {
+      if (currentSlide.value === getSlideCount.value) {
+        currentSlide.value = 1;
+        return;
+      }
+      currentSlide.value += 1;
+    };
+
+    // prev slide
+    const prevSlide = () => {
+      if (currentSlide.value === 1) {
+        currentSlide.value = 1;
+        return;
+      }
+      currentSlide.value -= 1;
+    };
+
+    // to match pagination with slide (slide to pagination is working )
+    const goToSlide = (index) => {
+      currentSlide.value = index + 1;
+    };
+
+    // autoplay
+    const autoPlay = () => {
+      setInterval(() => {
+        nextSlide();
+      }, timeoutDuration.value);
+    };
+
+    if (autoPlayEnabled.value) {
+      autoPlay();
+    }
+
+    onMounted(() => {
+      getSlideCount.value = document.querySelectorAll(".slide").length;
+      // console.log(getSlideCount.value);
+    });
+
+    return {
+      currentSlide,
+      getSlideCount,
+      nextSlide,
+      prevSlide,
+      goToSlide,
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import App from "./App.vue";
+import router from "./router";
 
 import "./assets/global.css";
 
 library.add(fas);
 
-createApp(App).component("fa", FontAwesomeIcon).mount("#app");
+createApp(App).use(router).component("fa", FontAwesomeIcon).mount("#app");
